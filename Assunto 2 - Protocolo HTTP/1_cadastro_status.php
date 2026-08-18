2f0aca093fe191398a3fe93468fe513547777018
Alyyfy <conta.secundariamente7282@gmail.com>
2026-08-18 16:11:19 -0300

    Assunto 2
    Introdução de material

---
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro - Status Codes</title>
+</head>
+<body>
+    <h1>Cadstro de Aluno (com status Code)</h1>
+    <form method = "post" action ="">
+         <label for = "nome">Nome:</label>
+         <input type = "text" name = "Nome" require><br><br>  
+         
+         <input for = "idade">Idade: require><br><br>
+
+         <button type="sumbit">Enviar</button>
+    </form>    
+
+</body>
+</html>
